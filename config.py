bfe17e12a4f74d68b9595f04335a2a3f4a96b4fa
Chang Liu <changliu1991@hotmail.com>
2018-07-11 17:12:05 +0800

    feat: add business card image upload and recognition

--- config.py
+++ config.py
@@ -4,8 +4,13 @@ class Config(object):
 
 class DevConfig(Config):
     DATABASE_URI = 'mysql://dementor:Messi#10@127.0.0.1:3306/test?charset=utf8'
-    LOG_FILE = '/Users/dementor/Documents/logs/xmarketing/website.log'
-    CRON_LOG_FILE = '/Users/dementor/Documents/logs/xmarketing/cron.log'
+    UPLOAD_FOLDER = '/Users/dementor/Documents/upload/xmarketing'
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+    PIC_URL = 'http://p3atcmc03.bkt.clouddn.com/'
+    AUTH_ORIGINAL = 'a=%s&b=test&k=%s&e=%s&t=%s&r=%s&f=%s'
+    APPID = '1252954035'
+    SECRET_ID = 'AKIDrdEeu1a0lLwNcfuN8h4SeigV9brygIEz'
+    SECRET_KEY = 'ra8UefW1OahrpbCbopdJo8IG4hASaYUH'
     PROFILE='dev'
 
 class TestConfig(Config):
--- xmarketing/views/general.py
+++ xmarketing/views/general.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 import datetime
-from flask import Blueprint, request
+import os
+import hashlib
+import hmac
+import base64
+import ocr
+from flask import Blueprint, request, redirect, url_for
 from xmarketing.db import Session, Visitor
 from xmarketing.util import Result
+from xmarketing import app
+from simplecrypt import encrypt
 import logging, json
 
 mod = Blueprint('general', __name__)
@@ -39,6 +46,38 @@ def visitor_record():
     Session.commit()
     print(json.dumps(Result().success().set_message('success').to_dict()))
     return json.dumps(Result().success().set_message('success').to_dict())
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/uploadorscan', methods=['GET', 'POST'])
+def upload_file():
+    busicard_uri = app.config['PIC_URL']
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = datetime.datetime.now().strftime("%Y%m%d%H%M%S")+'.'+file.filename.rsplit('.', 1)[1].lower()
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for('upload_file',filename=filename))
+            busicard_uri += filename
+            print('======' + busicard_uri + '======')
+            busicard_uri = 'http://p3atcmc03.bkt.clouddn.com/namecard1.jpg'
+            rec_str = ocr.businesscard_recognize(app.config['APPID'], busicard_uri)
+
+    return rec_str
+
+def recognition(pic_uri):
+    return ocr.businesscard_recognize(app.config['APPID'], pic_uri)
+
 
 @mod.route('/test', methods=['GET'])
 def test():
